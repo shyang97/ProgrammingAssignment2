5492a33857107b19cdada67f3a7f2785407138ee
shyang97 <45622957+shyang97@users.noreply.github.com>
2018-12-21 20:18:59 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,10 +4,10 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {# Input a matrix
-        i <- matrix()                       # Initialize i as a blank matrix, which will be the inverse of x
+        i <- NULL                          # Initialize i as NULL
         set <- function(y) {                # Set the value of the matrix
                 x <<- y                     # Assign to x the value of input argument
-                i <<- matrix()              # Reset the inverse matrix i as a blank matrix since x is newly assigned
+                i <<- NULL                  # Reset the inverse matrix i as NULL since x is newly assigned
         }
         get <- function() x                 # Retrieve x
         setinverse <- function(inverse) i <<- inverse   # Assign to i the value of input argument
@@ -22,7 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {# Input a matrix
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {            # Input a makeCacheMatrix object as an argument
-        i <- x$getinverse()                 # Assign to i the cached value, if there is not a cached value, the value of i will be NA
+        i <- x$getinverse()                 # Assign to i the cached value, if there is not a cached value, the value of i will be NULL
         if(!is.null(i)) {                   # If the value of i is not NA, i.e. there is a cached value, this simply returns the cached value without doing any calculation
                 message("getting cached data")
                 return(i)
